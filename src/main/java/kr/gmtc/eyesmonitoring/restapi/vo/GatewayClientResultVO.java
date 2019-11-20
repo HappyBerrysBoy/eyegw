92edf45e0bd09cfaa88bef92c339f9e5cd2d602f
happyberrysboy <elytselyts@gmail.com>
2019-11-20 17:11:51 +0900

    add_vo

---
+++ src/main/java/kr/gmtc/eyesmonitoring/restapi/vo/GatewayClientResultVO.java
@@ -0,0 +1,5 @@
+package kr.gmtc.eyesmonitoring.restapi.vo;
+
+public class GatewayClientResultVO {
+  // {"result":[{"Header":{"MsgGroup":0,"SendDT":"2019-10-22 17:43:25","MsgID":4},"Data":[{"ClientName":"LGS","Active":0,"IP":"127.0.0.1","Port":9904,"Connect":false,"SendBuffer":0,"ID":9904},{"ClientName":"RLYCLIENT","Active":0,"IP":"192.168.136.132","Port":9900,"Connect":false,"SendBuffer":0,"ID":9900},{"ClientName":"CLIENT#1","Active":0,"IP":"127.0.0.1","Port":9901,"Connect":false,"SendBuffer":0,"ID":9901},{"ClientName":"UGW","Active":0,"IP":"192.168.136.131","Port":9903,"Connect":false,"SendBuffer":0,"ID":9903}]}]}
+}
---
+++ src/main/java/kr/gmtc/eyesmonitoring/restapi/vo/StatusResultVO.java
@@ -0,0 +1,113 @@
+package kr.gmtc.eyesmonitoring.restapi.vo;
+
+// sample data
+// {
+// result: [
+// {
+// Header: { MsgGroup: 0, SendDT: "2019-10-22 16:42:27", MsgID: 1 },
+// Data: { Start: "2019-10-22 16:37:14", SendBuffer: 80, RecvBuffer: 90 }
+// }
+// ]
+// }
+
+public class StatusResultVO {
+  private HeaderVO Header;
+  private DataVO Data;
+
+  public StatusResultVO() {
+  }
+
+  public HeaderVO getHeader() {
+    return Header;
+  }
+
+  public void setHeader(HeaderVO header) {
+    Header = header;
+  }
+
+  public DataVO getData() {
+    return Data;
+  }
+
+  public void setData(DataVO data) {
+    Data = data;
+  }
+
+  class HeaderVO {
+    private int MsgGroup;
+    private String SendDT;
+    private int MsgID;
+
+    public HeaderVO() {
+    }
+
+    public int getMsgGroup() {
+      return MsgGroup;
+    }
+
+    public void setMsgGroup(int msgGroup) {
+      MsgGroup = msgGroup;
+    }
+
+    public String getSendDT() {
+      return SendDT;
+    }
+
+    public void setSendDT(String sendDT) {
+      SendDT = sendDT;
+    }
+
+    public int getMsgID() {
+      return MsgID;
+    }
+
+    public void setMsgID(int msgID) {
+      MsgID = msgID;
+    }
+  }
+
+  class DataVO {
+    private String Start;
+    private float SendBuffer;
+    private float isRecvBuffer;
+
+    public DataVO() {
+
+    }
+
+    public String getStart() {
+      return Start;
+    }
+
+    public void setStart(String start) {
+      Start = start;
+    }
+
+    public float getSendBuffer() {
+      return SendBuffer;
+    }
+
+    public void setSendBuffer(float sendBuffer) {
+      SendBuffer = sendBuffer;
+    }
+
+    public float getIsRecvBuffer() {
+      return isRecvBuffer;
+    }
+
+    public void setIsRecvBuffer(float isRecvBuffer) {
+      this.isRecvBuffer = isRecvBuffer;
+    }
+  }
+
+  public static void main(Object args) {
+    HeaderVO header = new HeaderVO();
+    DataVO data = new DataVO();
+
+    header.setMsgGroup(3);
+    System.out.println(header.getMsgGroup());
+
+    data.setStart("TEST");
+    System.out.println(data.getStart());
+  }
+}
